b790983dc35d5e04495cf8ba118ff3761e4ccc56
espiler <monkeysrawesome@gmail.com>
2015-04-13 12:43:56 -0700

    swap methods

--- methods.js
+++ methods.js
@@ -1,76 +1,50 @@
 module.exports = Database;
 
 function Database() {
-  this.data = {};
-  this.values = {};
-  this.dataChanges = {};
-  this.valueChanges = {};
-  this.transaction = 0;
-  this.inTransaction = false;
+  // Create data & values table stacks
+  this.data = [{}];
+  this.values = [{}];
 
-  this.updateValue = function(key, val, mod) {
-    mod = mod || 1;
-    this.values[val] ?
-      this.values[val]+= 1*mod :
-      this.values[val] = 1*mod;
-    if (this.transaction) {
-      this.valueChanges[this.transaction][this.data[key]] ?
-        this.valueChanges[this.transaction][this.data[key]]+= 1*mod :
-        this.valueChanges[this.transaction][this.data[key]] = 1*mod;
-    }
-  }
-  
-  this.set = function(key, val) {
-    if (this.data[key]) { this.updateValue(key, this.data[key], -1); }
-    if (this.transaction && !this.dataChanges[this.transaction][key]) { this.dataChanges[this.transaction][key] = this.data[key] || 'delete'}
-    this.data[key] = val;
-    this.updateValue(key, val);
+  // Update data & values table for current transaction
+  this.set = function(key, val) {    
+    if (this.data[this.data.length-1][key]) { this.values[this.values.length-1][this.data[this.data.length-1][key]]--; }
+    this.values[this.values.length-1][val] ?
+      this.values[this.values.length-1][val]++ :
+      this.values[this.values.length-1][val] = 1;
+    this.data[this.data.length-1][key] = val;
   }
 
   this.get = function(key) {
-    return this.data[key] || 'NULL';
+    return this.data[this.data.length-1][key] || 'NULL';
   }
 
+  // Update data & values table for current transaction
   this.unset = function(key) {
-    if (this.data[key]) {
-      this.updateValue(key, this.data[key], -1);
-    }
-    if (this.transaction && !this.dataChanges[this.transaction][key]) { this.dataChanges[this.transaction][key] = this.data[key] || 'delete'}
-    delete this.data[key];
+    if (this.data[this.data.length-1][key]) { this.values[this.values.length-1][this.data[this.data.length-1][key]]--; }
+    this.data[this.data.length-1][key] = null;
   }
 
   this.numEqualTo = function(val) {
-    return this.values[val] || 0;
+    return this.values[this.values.length-1][val] || 0;
   }
 
+  // Push new subclassed data & values tables with to stacks
   this.begin = function() {
-    this.inTransaction = true;
-    this.transaction++;
-    this.dataChanges[this.transaction] = {};
-    this.valueChanges[this.transaction] = {};
+    this.data.push(Object.create(this.data[this.data.length-1]));
+    this.values.push(Object.create(this.values[this.values.length-1]));
   }
 
+  // Pop data & values tables from stacks to return to prev state
   this.rollback = function() {
-    if (!this.inTransaction) { return "NO TRANSACTION"; }
-    for (var key in this.dataChanges[this.transaction]) {
-      this.dataChanges[this.transaction][key] === 'delete' ?
-        delete this.data[key] :
-        this.data[key] = this.dataChanges[this.transaction][key];
-    }
-    for (var value in this.valueChanges[this.transaction]) {
-      this.values[value] -= this.valueChanges[this.transaction][value];
-    }
-    delete this.dataChanges[this.transaction];
-    delete this.valueChanges[this.transaction];
-    this.transaction--;
-    if (!this.transaction) { this.inTransaction = false; }
+    if (this.data.length === 1) { return "NO TRANSACTION"; }
+    this.data.pop();
+    this.values.pop();
   }
 
+  // Reset stack with most recent transaction at 0
   this.commit = function() {
-    if (!this.inTransaction) { return "NO TRANSACTION"; }
-    this.transaction = 0;
-    this.inTransaction = false;
-    this.dataChanges = {};
-    this.valueChanges = {};
+    if (this.data.length === 1) { return "NO TRANSACTION"; }
+    this.data = [this.data[this.data.length-1]];
+    this.values = [this.values[this.values.length-1]];
   }
 }
